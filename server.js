ed14c5bf16d068f02b18c91395aa1779b8cdaf28
Kaiden Smith-Wood <100881423+Ducci129@users.noreply.github.com>
2023-11-14 23:25:19 +0000

    show client SocketID and other users ClientID

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
+// server.js
+
 const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketIo = require('socket.io');
 const fs = require('fs');
-const uuid = require('uuid');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const port = 3000;
+const port = 3001
+
+const TIMER_DATA_FILE = 'timerData.json';
+const TIMER_UPDATE_INTERVAL = 1000;
+
+// Validate timer update interval
+if (!Number.isInteger(TIMER_UPDATE_INTERVAL) || TIMER_UPDATE_INTERVAL <= 0) {
+  console.error('Invalid timer update interval. Using default value.');
+}
 
 let timerValueInSeconds = loadTimerValueFromFile() || 0;
-let users = {}; // Keep track of users, their unique identifiers, and names
+let cursors = {}; // Keep track of cursors
 
 // Serve static files (like index.html) from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -31,92 +40,35 @@ setInterval(() => {
   timerValueInSeconds += 1;
   io.emit('timerUpdate', timerValueInSeconds); // Send timer updates to all connected clients
   saveTimerValueToFile(timerValueInSeconds);
-}, 1000);
+}, TIMER_UPDATE_INTERVAL);
 
 // Socket.io connection event
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Generate a unique identifier for the user (you can replace this with IP if needed)
-  const userId = generateUserId();
-
-  // Set a default name for the user
-  let userName = `User${Object.keys(users).length + 1}`;
-
-  // Send the current timer value, user name, and user id to the newly connected client
+  // Clear all cursors and send the current timer value to the newly connected client
+  cursors = {};
   socket.emit('timerUpdate', timerValueInSeconds);
-  socket.emit('setUserData', { userId, userName });
 
-  // Associate the user's unique identifier, name, and socket ID
-  users[socket.id] = { userId, userName, ip: socket.handshake.address };
+  Object.entries(cursors).forEach(([socketId, position]) => {
+    socket.emit('cursorPosition', { socketId, position });
+  });
 
   // Listen for cursor position updates from clients
   socket.on('cursorPosition', (position) => {
-    // Broadcast the cursor position to all connected clients
-    io.emit('cursorPosition', { userId, position });
+    if (isValidCursorPosition(position)) {
+      io.emit('cursorPosition', { socketId: socket.id, position });
+      cursors[socket.id] = position;
+    } else {
+      console.error(`Invalid cursor position received from socket ${socket.id}:`, position);
+    }
   });
 
   // Handle disconnection
   socket.on('disconnect', () => {
     console.log('A user disconnected');
-    // Remove the user from the users object
-    delete users[socket.id];
-    io.emit('cursorDisconnect', userId);
-    io.emit('chatMessage', { userId: 'System', message: `${userName} has left the chat.` });
-  });
-
-  // Send existing cursors and user data to a newly connected client
-  io.to(socket.id).emit('currentCursors', getUsersCursors());
-  io.to(socket.id).emit('currentUsers', getUsersData());
-});
-
-// Send existing cursors to a newly connected client
-function getUsersCursors() {
-  const cursors = [];
-  for (const [socketId, { userId, position }] of Object.entries(users)) {
-    if (position) {
-      cursors.push({ userId, position });
-    }
-  }
-  return cursors;
-}
-
-// Send existing user data to a newly connected client
-function getUsersData() {
-  const userData = [];
-  for (const { userId, userName, ip } of Object.values(users)) {
-    userData.push({ userId, userName, ip });
-  }
-  return userData;
-}
-
-// Function to generate a unique user identifier (you can replace this with IP if needed)
-function generateUserId() {
-  return uuid.v4();
-}
-
-// Function to save the timer value to a file
-function saveTimerValueToFile(value) {
-  const data = JSON.stringify({ timerValue: value });
-  fs.writeFileSync('timerData.json', data);
-}
-
-// Function to load the timer value from a file
-function loadTimerValueFromFile() {
-  try {
-    const data = fs.readFileSync('timerData.json', 'utf8');
-    const parsedData = JSON.parse(data);
-    return parsedData.timerValue;
-  } catch (error) {
-    console.error('Error loading timer value from file:', error);
-    return null;
-  }
-}
-
-// Listen for chat messages
-io.on('connection', (socket) => {
-  socket.on('chatMessage', (message) => {
-    io.emit('chatMessage', { userId: users[socket.id].userId, message });
+    delete cursors[socket.id];
+    io.emit('cursorDisconnect', socket.id);
   });
 });
 
@@ -124,3 +76,50 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// Function to validate cursor position
+function isValidCursorPosition(position) {
+  return (
+    position &&
+    typeof position === 'object' &&
+    typeof position.x === 'number' &&
+    typeof position.y === 'number'
+  );
+}
+
+// Function to validate and sanitize file name
+function sanitizeFileName(fileName) {
+  const fileParts = fileName.split('.');
+  const baseName = fileParts[0];
+  const extension = fileParts.length > 1 ? `.${fileParts.slice(1).join('.')}` : '';
+
+  // Ensure the base name is safe and adheres to any naming conventions
+  const sanitizedBaseName = baseName.replace(/[^a-zA-Z0-9_]/g, '_');
+
+  return `${sanitizedBaseName}${extension}`;
+}
+
+// Function to save the timer value to a file
+function saveTimerValueToFile(value) {
+  const data = JSON.stringify({ timerValue: value });
+  const sanitizedFileName = sanitizeFileName(TIMER_DATA_FILE);
+  fs.writeFileSync(sanitizedFileName, data);
+}
+
+// Function to load the timer value from a file
+function loadTimerValueFromFile() {
+  try {
+    const sanitizedFileName = sanitizeFileName(TIMER_DATA_FILE);
+    const data = fs.readFileSync(sanitizedFileName, 'utf8');
+    const parsedData = JSON.parse(data);
+    return isValidTimerValue(parsedData.timerValue) ? parsedData.timerValue : null;
+  } catch (error) {
+    console.error(`Error loading timer value from file '${TIMER_DATA_FILE}':`, error);
+    return null;
+  }
+}
+
+// Function to validate timer value
+function isValidTimerValue(value) {
+  return typeof value === 'number';
+}
